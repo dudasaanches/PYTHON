2434361b71db38d65c2b9bbb5f5bc3a90b1e6185
dudasaanches <124930071+dudasaanches@users.noreply.github.com>
2023-03-11 16:57:38 -0300

    Add files via upload

---
+++ python/Letra.py
@@ -0,0 +1,6 @@
+letra = input("Informe o letra: ")
+
+if(letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u"):
+    print("Sua letra é Vogal")
+else:
+    print("Sua letra é Consoante")
