d69ff8fcb4ed707edf831eff5e471f3159dc1ab6
dudasaanches <124930071+dudasaanches@users.noreply.github.com>
2023-04-08 16:50:57 -0300

    Add files via upload

---
+++ PYTHON/aulas/desafio.py
@@ -0,0 +1,8 @@
+while True:
+    var = input("Você gosta de Python? ")
+    var.upper()
+    if var == "SIM":
+        print("Resposta certa!")
+        break
+    else:
+        print("Esta não é a resposta certa, tente novamente")
